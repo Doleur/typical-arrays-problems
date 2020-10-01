9103f3bddda04b1fbeef325a2315f8a9e45674fd
Doleur <voleur16@gmail.com>
2020-10-01 18:18:09 +0300

    fix: implement max, min and avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let minN = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        for (let i of array) {
+            minN = Math.min(i, minN)
+        }
+        return minN;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let maxN = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        for (let i of array) {
+            maxN = Math.max(i, maxN)
+        }
+        return maxN;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sumAll = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        for (let i of array) {
+            sumAll = sumAll + i;
+        }
+        return (sumAll / array.length);
+    }
 }
